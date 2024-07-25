59c4dad0da04d844681f9707f141513bf7b7f9cc
tyeckh <117521882+tyeckh@users.noreply.github.com>
2024-07-25 11:10:19 +0800

    Typing Effect

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ document.querySelector('.logo').addEventListener('click', function(event) {
     event.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+const text = "Chan Kit Ho";
+let index = 0;
+const typingSpeed = 200; 
+const titleElement = document.getElementById("typing-text");
+
+function type() {
+  if (index < text.length) {
+    titleElement.innerHTML += text.charAt(index);
+    index++;
+    setTimeout(type, typingSpeed);
+  }
+}
+
+function resetAndType() {
+  index = 0;
+  titleElement.innerHTML = " ";
+  type();
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      resetAndType();
+    }
+  });
+}, {
+  threshold: 1
+});
+
+observer.observe(document.querySelector(".section__text"));
